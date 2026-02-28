ac87ce32787f2660bf42980e3729e0764144a2a6
anuragparashar26 <anuragparashar5025@gmail.com>
2026-02-28 22:50:45 +0530

    final fix

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://sqlstudiocipherschools-production.up.railway.app"
+        "https://sql-studio-cipher-schools-pi.vercel.app/"
     ]
 }));
 app.use(express.json());
